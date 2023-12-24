e4739ca69f4880d6e54211b5462caec2ceb05763
= <panchalurvil160@gmail.com>
2023-12-24 17:50:00 +0530

    Initial Commit

---
+++ Senti.py
@@ -0,0 +1,27 @@
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+nltk.download('vader_lexicon')
+
+def Sentiment_Analyzer(text):
+    sia = SentimentIntensityAnalyzer()
+
+    sentiment_score = sia.polarity_scores(text)
+
+    compound_scores = sentiment_score['compound']
+
+    if compound_scores >= 0.05:
+        sentiment = 'Positive'
+
+    elif compound_scores <= -0.05:
+        sentiment = "Negative"
+
+    else:
+        sentiment = "Neutral"
+
+    return sentiment , compound_scores
+
+if __name__ == '__main__':
+    s,c = Sentiment_Analyzer('I am not very Bad')
+
+    print(s,c)
---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask , render_template, request
+import Senti
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET', 'POST'])
+def Home():
+    return render_template('home.html')
+
+@app.route("/analyzer" , methods=['POST'])
+def Analyzer():
+    if request.method=='POST':
+        text = request.form['txt']
+    
+
+    # print(text)
+    sentiment , compoundScore = Senti.Sentiment_Analyzer(text)
+
+    return render_template('home.html' ,text=text,  s=sentiment,c=compoundScore)
+
+# if __name__ == '__main__':
+#     app.run(debug=True,host='0.0.0.0')
